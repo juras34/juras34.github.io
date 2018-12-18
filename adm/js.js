5165bf852c61546fd8fcaa17c9a10503e7f3f4e1
juras22 <45906537+juras-org@users.noreply.github.com>
2018-12-18 20:20:36 +0100

    masc colour

--- adm/js.js
+++ adm/js.js
@@ -7,9 +7,9 @@ ctx.fillRect(150, 100, 100, 50);
 // ctx.fillRect(x, y, width, height);
 ctx.clearRect(0,0, 400, 200); //стирает canvas
 
-ctx.strokeStyle = "green";
+ctx.strokeStyle = "red";
 ctx.lineWidth = "10";
 ctx.rect(50,10, 100, 100);
 ctx.stroke();
-ctx.fillStyle = "orange";
+ctx.fillStyle = "black";
 ctx.fill();
