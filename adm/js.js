8fbb18891b418cda929d6cb0cdbafaf86a668a07
juras22 <45906537+juras-org@users.noreply.github.com>
2018-12-18 21:00:27 +0100

    canvas add

--- adm/js.js
+++ adm/js.js
@@ -1,15 +1,10 @@
 var canvas = document.getElementById('c1');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = 'red';
-ctx.fillRect(100, 50, 150, 75);
+/*
 ctx.fillStyle = 'blue';
-ctx.fillRect(150, 100, 100, 50);
-// ctx.fillRect(x, y, width, height);
-ctx.clearRect(0,0, 400, 200); //стирает canvas
+ctx.fillRect(100, 50, 50, 50);
+*/
 
-ctx.strokeStyle = "red";
-ctx.lineWidth = "10";
-ctx.rect(50,10, 100, 100);
-ctx.stroke();
-ctx.fillStyle = "black";
-ctx.fill();
+ctx.strokeStyle = 'red';
+ctx.moveTo(100, 50);
+ctx.lineTo(150, 100);
